fc981612ea3380774dcd5cec23aca8bba5d85fc8
Matej Bašić <matej.basic@outlook.com>
2023-10-20 13:14:09 +0200

    Added svelte benchmark

---
+++ static-website/svelte-bench/src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+	const time = new Date();
+	const requestTime = window.performance.timing.requestStart;
+	const fullTime = time - requestTime;
+</script>
+
+<main>
+	<div class='timer-header'>
+		Simple Svelte Benchmark
+	</div>
+	<div class='timer'>Time from the start of the request to when the page loaded: {fullTime} ms</div>
+</main>
