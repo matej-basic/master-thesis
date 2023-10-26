4e22a159f5f85eeb9de2a8cd77ba6e9d2dd9f3a3
Matej Bašić <matej.basic@outlook.com>
2023-10-26 19:42:54 +0200

    Added port to application

--- api/python-flask/main.py
+++ api/python-flask/main.py
@@ -1,9 +1,12 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
+app_port = os.getenv('PORT')
 
 @app.route('/benchmark')
 def helloworld():
     return "Simple Python Flask benchmark"
 
-application = app
+if __name__ == "__main__":
+    app.run(port=app_port)
