d3a11bf0423ccb07611f5da2473e156d33e7eefb
Matej Bašić <matej.basic@outlook.com>
2023-10-26 19:08:37 +0200

    Switched port from hardcoded to ENV variable

--- api/javascript-express/server.js
+++ api/javascript-express/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.get('/benchmark', (req, res) => {
     res.send('Simple ExpressJS Benchmark');
