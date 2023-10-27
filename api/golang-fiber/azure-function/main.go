2aef35ef3dd754cd2eca6c4237f35182739b363b
matej <matej@react.oberin.cloud>
2023-10-27 19:36:53 +0200

    Added Azure function

---
+++ api/golang-fiber/azure-function/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func getPort() string {
+	port := os.Getenv("FUNCTIONS_CUSTOMHANDLER_PORT")
+	if port == "" {
+		port = ":3000"
+	} else {
+		port = ":" + port
+	}
+
+	return port
+}
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/benchmark", func(c *fiber.Ctx) error {
+		return c.SendString("Simple Golang Fiber Benchmark")
+	})
+
+	app.Listen(getPort())
+}
