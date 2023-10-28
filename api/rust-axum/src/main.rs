8167aec5a6bfc7c4afec376655a69ddef7d24b6c
Matej Bašić <matej.basic@outlook.com>
2023-10-28 17:11:42 +0200

    Reverted non function deployment back to hardoced port number

--- api/rust-axum/src/main.rs
+++ api/rust-axum/src/main.rs
@@ -5,15 +5,7 @@ use std::env;
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/benchmark", get(handler));
-    let port = env::var_os("FUNCTIONS_CUSTOMHANDLER_PORT")
-        .map(|value| {
-		value.to_string_lossy()
-   		     .parse::<i32>()
-		     .unwrap_or(3000)
-	})
-	.unwrap_or(3000);
-
-    let addr = SocketAddr::from(([0,0,0,0], port as u16));
+    let addr = SocketAddr::from(([0,0,0,0], 3000));
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
