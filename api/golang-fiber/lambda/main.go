39a3e0e02da3adbb3c12a40c35d0e4fae8c1bd95
Matej Bašić <matej.basic@outlook.com>
2023-10-28 15:41:28 +0200

    Added fiber lambda code

---
+++ api/golang-fiber/lambda/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func HandleRequest() (string, error){
+	return "Simple Golang Lambda benchmark", nil
+}
+func main() {
+	lambda.Start(HandleRequest)
+}
