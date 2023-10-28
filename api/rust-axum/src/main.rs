e50d6f3a84f5d0af2452dd45907c75f254258bd6
matej <matej@react.oberin.cloud>
2023-10-28 16:17:56 +0200

    Added looking for custom port on Axum,

--- api/rust-axum/src/main.rs
+++ api/rust-axum/src/main.rs
@@ -1,9 +1,14 @@
 use axum::{routing::get, Router};
 use std::net::SocketAddr;
+use std::env;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/benchmark", get(handler));
+    let port = match env::var("FUNCTIONS_CUSTOMHANDLER_PORT") {
+        Some(v) => v.into_int().unwrap(),
+        None => "3000"
+    };
 
     let addr = SocketAddr::from(([0,0,0,0], 3000));
 
