6154f15b12b70108eb67fc471d7e9c78af5313cb
Matej Bašić <matej.basic@outlook.com>
2023-10-28 16:34:33 +0200

    Added check for env variable

--- api/rust-axum/src/main.rs
+++ api/rust-axum/src/main.rs
@@ -5,12 +5,15 @@ use std::env;
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/benchmark", get(handler));
-    let port = match env::var("FUNCTIONS_CUSTOMHANDLER_PORT") {
-        Some(v) => v.into_int().unwrap(),
-        None => "3000"
-    };
+    let port = env::var_os("FUNCTIONS_CUSTOMHANDLER_PORT")
+        .map(|value| {
+		value.to_string_lossy()
+   		     .parse::<i32>()
+		     .unwrap_or(3000)
+	})
+	.unwrap_or(3000);
 
-    let addr = SocketAddr::from(([0,0,0,0], 3000));
+    let addr = SocketAddr::from(([0,0,0,0], port as u16));
 
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
