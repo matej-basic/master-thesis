de74093584742c52573c13a8f29f606de19cdd36
matej <matej@react.oberin.cloud>
2023-10-28 13:14:46 +0200

    Added ExpressJS amplify config and Gin Fly configu

---
+++ api/javascript-express/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "express": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "express": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
