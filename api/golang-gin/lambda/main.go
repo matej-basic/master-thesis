20f2d236c0555c7c50b7dec5b1222c1d3e535b92
Matej Bašić <matej.basic@outlook.com>
2023-10-29 16:24:01 +0100

    Refactor

--- api/golang-gin/lambda/main.go
+++ api/golang-gin/lambda/main.go
@@ -1,34 +1,37 @@
 package main
 
 import (
-	"log"
 	"context"
+	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/awslabs/aws-lambda-go-api-proxy/gin"
-	"github.com/gin-gonic/gin"
+	fiberadapter "github.com/awslabs/aws-lambda-go-api-proxy/fiber"
+	"github.com/gofiber/fiber/v2"
 )
 
-var ginLambda *ginadapter.GinLambda
+var fiberLambda *fiberadapter.FiberLambda
 
+// init the Fiber Server
 func init() {
-	// stdout and stderr are sent to AWS CloudWatch Logs
-	log.Printf("Gin cold start")
-	r := gin.Default()
-	r.GET("/benchmark", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"msg": "Simple Gin Benchmark"})
+	log.Printf("Fiber cold start")
+	var app *fiber.App
+	app = fiber.New()
+
+	app.Get("/benchmark", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
 	})
 
-	ginLambda = ginadapter.New(r)
+	fiberLambda = fiberadapter.New(app)
 }
 
+// Handler will deal with Fiber working with Lambda
 func Handler(ctx context.Context, req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	// If no name is provided in the HTTP request body, throw an error
-	return ginLambda.ProxyWithContext(ctx, req)
+	return fiberLambda.ProxyWithContext(ctx, req)
 }
 
 func main() {
+	// Make the handler available for Remote Procedure Call by AWS Lambda
 	lambda.Start(Handler)
 }
